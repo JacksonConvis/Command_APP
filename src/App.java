54af7ccdf010604958e4c34648b9455aa1a35c51
Jack Convis <jackconvis@gmail.com>
2022-10-03 21:13:52 -0400

    hopefully no runtime errors

--- src/App.java
+++ src/App.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        ArrayList<Variable> Varis = new ArrayList<Variable>();
-        Variable bobVariable = new Variable("Bob", "theBigMan");
+        System.out.println("DECLARE YOUR VARIABLE'S TYPE:");
+        String type = scan.nextLine();
 
+        System.out.println("DECLARE YOUR VARIABLE'S NAME");
+        String name = scan.nextLine();
+
+        ArrayList<Variable> variables = new ArrayList<Variable>();
+        Variable vrbl = new Variable(name, type);
+        variables.add(vrbl);
+
+        App.printVariablesNames(variables);
+        vrbl.getBits();
+        vrbl.getTotalBits(variables);
     }
 
     public static void printVariablesNames(ArrayList<Variable> variables) {
