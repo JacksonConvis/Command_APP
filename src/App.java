3f3ea5fe1d9fe0793bf9cbb395b979f5ed092e2f
Jack Convis <jackconvis@gmail.com>
2022-10-03 20:55:26 -0400

    app method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        ArrayList<Variable> Varis = new ArrayList<Variable>();
+        Variable bobVariable = new Variable("Bob", "theBigMan");
+
+    }
+
+    public static void printVariablesNames(ArrayList<Variable> variables) {
+
+        for (int i = 0; i < variables.size(); i++) {
+            System.out.println(variables.get(i).getName());
+        }
     }
 }
